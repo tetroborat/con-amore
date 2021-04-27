a0195d514661aa6a3ec05d0591c94ee139e12f67
tetroborat <muchomedyanov@ya.ru>
2021-04-27 17:46:15 +0300

    Шаманил с подгрузкой медиа с Amazon S3

--- amore/amore/settings.py
+++ amore/amore/settings.py
@@ -9,9 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
-from os import environ
-from os.path import join
+from os.path import join, dirname
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,16 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(fks+q)h*4^!4&8przyp^c9!-5)ub7(+27aukaq+oc=gcc)^ij'
 
-AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_URL = environ.get('AWS_URL')
-# AWS_DEFAULT_ACL = None
-# AWS_S3_REGION_NAME = 'eu-west-1'
-# AWS_S3_SIGNATURE_VERSION = 's3v4'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -43,17 +33,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
+
+    'django_cleanup',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'django_cleanup',
-    'storages',
-
-    'main_app'
 ]
 
 MIDDLEWARE = [
@@ -130,13 +119,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_URL = AWS_URL + '/media/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 STATIC_URL = '/static/'
 STATIC_ROOT = join(BASE_DIR, 'static_cdn')
 STATICFILES_DIRS = [join(BASE_DIR, 'static')]
 
-CART_SESSION_ID = 'cart'
+MEDIA_ROOT = join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+django_heroku.settings(locals())
 
-django_heroku.settings(locals(), staticfiles=False)
+CART_SESSION_ID = 'cart'
